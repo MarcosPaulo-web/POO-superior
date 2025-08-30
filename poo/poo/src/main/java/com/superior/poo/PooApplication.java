ed7f27535a863b58cd0b1b5992f17800aa86b64c
MarcosPaulo-web <marcospaulobitt@gmail.com>
2025-08-29 21:59:32 -0300

    Featuer : Hello word e adicionamos a spring web

--- poo/poo/src/main/java/com/superior/poo/PooApplication.java
+++ poo/poo/src/main/java/com/superior/poo/PooApplication.java
@@ -2,12 +2,21 @@ package com.superior.poo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication//permite rodar o string
+@RestController//permite criar corpo html
 public class PooApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PooApplication.class, args);
 	}
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello world";
+    }
 
 }
